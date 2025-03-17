8dde68ae232af8149495198b1c6cee836218ca21
Ramka <munasipovaramila6@gmail.com>
2025-03-17 10:00:30 +0300

    19.03

--- practika1/Assets/Assets/с#/ShootingScript.cs
+++ practika1/Assets/Assets/с#/ShootingScript.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class ShootingScript : MonoBehaviour
 {
     public Transform firePoint;
+
+    public GameObject bulletPrefab;
+    public GameObject canvas;
     void Update()
     {
         if (Input.GetMouseButton(1))
@@ -13,10 +16,17 @@ public class ShootingScript : MonoBehaviour
 
     void Shoot()
     {
+        GameObject bulletInstance = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        bulletInstance.transform.parent = canvas.transform;
         RaycastHit2D hit = Physics2D.Raycast(firePoint.position, firePoint.forward);
         if (hit)
         {
             Debug.Log("Hit " + hit.collider.gameObject);
+            Enemy enemy = hit.collider.gameObject.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.GetDamage();
+            }
         }
     }
 }
